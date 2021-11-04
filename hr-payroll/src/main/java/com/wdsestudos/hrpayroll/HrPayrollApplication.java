0691567bb164df458bc559662cfe00e5799dff98
Washington <washingtonds.87@gmail.com>
2021-11-04 15:33:59 -0300

    Feign Client

--- hr-payroll/src/main/java/com/wdsestudos/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/wdsestudos/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,15 @@ package com.wdsestudos.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(HrPayrollApplication.class, args);
 	}
 
--- hr-payroll/src/main/java/com/wdsestudos/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/wdsestudos/hrpayroll/services/PaymentService.java
@@ -1,29 +1,21 @@
 package com.wdsestudos.hrpayroll.services;
 
+import com.wdsestudos.hrpayroll.WorkerFeignClient;
 import com.wdsestudos.hrpayroll.entities.Payment;
 import com.wdsestudos.hrpayroll.entities.Worker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    @Autowired
-    private RestTemplate restTemplate;
+   @Autowired
+   private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(),worker.getDailyIncome(), days);
     }
 }
